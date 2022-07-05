c870275516dde4e7b9a6040b84ec742e15344d21
Mo Yang <moyang313@gmail.com>
2022-07-05 00:13:38 -0400

    add footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from './components/About';
 import Gallery from './components/Gallery';
 import ContactForm from './components/Contact';
 import Resume from './components/Resume';
+import Footer from './components/Footer';
 
 function App() {
   const [categories] = useState([
@@ -47,6 +48,7 @@ function App() {
           <ContactForm></ContactForm>
         )}
       </main>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+  return (
+      <div className="footer">
+              <a href="https://github.com/moyangdev" target="_blank" rel="noreferrer"><img src="https://img.icons8.com/color/48/000000/github-2.png" alt="GitHub" className="icon"/></a>
+              <a href="https://www.linkedin.com/in/myang3/" target="_blank" rel="noreferrer"><img src="https://img.icons8.com/color/48/000000/linkedin.png" alt="LinkedIn" className="icon"/></a>
+      </div>
+  )
+}
+
+export default Footer;
